662b2d490aee6631499a07a4bcd9eab5c74517bb
Abhishek Gorai <gorai.abhi@gmail.com>
2021-12-25 23:03:02 +0530

    refactored reducer to separate file.

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from "react";
+import { AppReducer } from "./AppReducer";
 
 const initialState = {
 	budget: 2000,
@@ -29,30 +30,3 @@ export const AppProvider = (props) => {
     );
 }
 
-const AppReducer = (state, action) => {
-	switch (action.type) {
-		case 'ADD_EXPENSE':
-            return {
-                ...state,
-                expenses: [ ...state.expenses, action.payload ],
-            }
-        case 'DELETE_EXPENSE':  
-            return {
-                ...state,
-                expenses: state.expenses.filter((expense) => expense.id !== action.payload),
-            }
-        case 'EDIT_BUDGET':
-            return {
-                ...state,
-                editBudgetFlag: action.payload,
-            }
-        case 'UPDATE_BUDGET':
-            return {
-                ...state,
-                budget: action.payload,
-                editBudgetFlag: action.editBudgetFlag,
-            }
-        default:
-			return state;
-	}
-};
